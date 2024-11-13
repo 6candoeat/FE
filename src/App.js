cbe2024c6df47e2c36dc9c92f98d5c93cae9627f
SuhJiyeon <jywhdk0320@naver.com>
2024-11-13 19:58:29 +0900

    Add menues Img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Main from './pages/Main';
-import RegistrationTypeSelect from './pages/RegistrationTypeSelect'
-import Mypage from './pages/Mypage';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+import RegistrationTypeSelect from "./pages/RegistrationTypeSelect";
+import Mypage from "./pages/Mypage";
+import Menues from "./pages/Menues";
 
 function App() {
   return (
@@ -12,8 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
-        <Route path='/registration' element={<RegistrationTypeSelect />} />
-        <Route path='/mypage' element={<Mypage />} />
+        <Route path="/registration" element={<RegistrationTypeSelect />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/menue" element={<Menues />} />
       </Routes>
     </Router>
   );
