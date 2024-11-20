23464cadd6a9da20390c1deacd8d31a72fc667ee
삼성이온2\sseo2 <sseo2732@gmail.com>
2024-11-21 01:10:11 +0900

    feat: 광고 슬라이드 효과 추가

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import '../../styles/banner.scss';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "../../styles/banner.scss";
+
+// Swiper 모듈 추가
+import { Autoplay, Pagination } from "swiper/modules";
 
 const Banner = () => {
+  const banners = [
+    require("../../assets/banner.png"),
+    require("../../assets/banner2.png"),
+    require("../../assets/banner3.png"),
+  ];
+
   return (
-    <div className="banner">
-      <img src={require('../../assets/banner.png')} alt="Banner" />
+    <div className="banner-slider">
+      <Swiper
+        modules={[Autoplay]}
+        autoplay={{
+          delay: 3000, // 3초 간격
+          disableOnInteraction: false, // 사용자 상호작용 후에도 자동 재생
+        }}
+        pagination={{ clickable: true }}
+        loop={true} // 무한 슬라이드
+        className="swiper-container"
+      >
+        {banners.map((banner, index) => (
+          <SwiperSlide key={index}>
+            <img src={banner} alt={`Banner ${index + 1}`} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
-/* */
 
 export default Banner;
